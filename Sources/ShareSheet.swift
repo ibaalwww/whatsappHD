e8387b8cf58ce8af1482e2b88f29d153df0efbac
ibaalwww <ibaalwww@gmail.com>
2026-09-03 03:54:51 +0700

    Update ShareSheet.swift

--- Sources/ShareSheet.swift
+++ Sources/ShareSheet.swift
@@ -2,10 +2,15 @@ import SwiftUI
 import UIKit
 
 struct ShareSheet: UIViewControllerRepresentable {
-    let activityItems: [Any]
+    var activityItems: [Any]
+    var applicationActivities: [UIActivity]? = nil
 
     func makeUIViewController(context: Context) -> UIActivityViewController {
-        UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+        let controller = UIActivityViewController(
+            activityItems: activityItems,
+            applicationActivities: applicationActivities
+        )
+        return controller
     }
 
     func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {}
